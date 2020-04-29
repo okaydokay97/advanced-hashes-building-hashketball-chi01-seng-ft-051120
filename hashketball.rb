7494a7c2c31a6d8031cdaade12145de2adbfc15c
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-04-29 01:46:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,26 @@
-# Write your code here!
+require 'pry'
+def game_hash
+  game = {}
+  game[:home] = {}
+  game[:home][:team_name] = "Brooklyn Nets"
+  game[:home][:colors] = ["Black", "White"]
+  game[:home][:players]= [
+    {:player_name => ["Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"]},
+    {:number => ["0", "30", "11", "1", "31"]},
+    {:shoe =>[]},
+    {:points =>[]},
+    {:rebounds =>[]},
+    {:assists =>[]},
+    {:steals =>[]},
+    {:blocks =>[]},
+    {:slam_dunks =>[]}
+  ]
+  
+  
+  game[:away] = {}
+  game[:away][:team_name] = "Charlotte Hornets"
+  game[:away][:colors] = ["Turquoise", "Purple"]
+  game[:away][:players]= [{}]
+
+  game
+end
