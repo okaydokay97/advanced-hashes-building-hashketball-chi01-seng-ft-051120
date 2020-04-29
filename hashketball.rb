1e5db2d939a1e8b557b394cfe01edab8711305d6
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-04-29 01:54:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,15 @@ def game_hash
   game[:home][:team_name] = "Brooklyn Nets"
   game[:home][:colors] = ["Black", "White"]
   game[:home][:players] = [
-    {}
+  :player_name
+  :number
+  :shoe
+  :points
+  :rebounds
+  :assists
+  :steals
+  :blocks
+  :slam_dunks{}
   ]
   
   
